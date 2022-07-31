4ec65edcffc416d3d94e8dc56c29c296c8d9cfda
Alkylaid <alkylaid@gmail.com>
2022-07-30 21:05:14 -0500

    Improve styling for home page

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,9 @@ function App() {
     <div className="container">
       <div id="hero-image">
       <Navbar />
+      <h1>Come experience the savings.</h1>
+      <h2>Get up to <span style={{color: 'rgb(225, 43, 70)'}}>65%</span> off on your favorite brands.</h2>
+      <button id="check-it-button">Check It Out</button>
       </div>
       <Home />
     </div>
